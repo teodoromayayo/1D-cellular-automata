baa664f82f9737a40e80d6bd1755c797a6afcdab
Teodoro Mayayo <87387120+teodoromayayo@users.noreply.github.com>
2023-04-29 20:02:40 +0200

    Add files via upload

--- CA_1D.py
+++ CA_1D.py
@@ -57,21 +57,3 @@ def plot_cellular_automata(cellular_automata):
     plt.imshow(cellular_automata, cmap='binary')
     plt.axis('off')
     plt.show()
-
-#----------------------Assigning values to variagles----------------------------------
-
-iterations = 256
-size = 513
-rule = 30
-# Survival probability is equal to 1 if the cellular automata is deterministic
-survival_prob = 1
-# If a standard type is implemented, size should be bigger or equal to 2*iteration+1 becaus is coded for periodic boundarie and we would avoid collisions
-type = 'random'
-# Is the probability of appearing living cells when considering random conditions in the initial array
-generation_probability = 0.5
-
-#-----------------------Implementation of the code------------------------------------
-
-# Implememntation of the cellular automata and plot
-CA = cellular_automata(iterations, size, rule, survival_prob, type, generation_probability)
-plot_cellular_automata(CA)
